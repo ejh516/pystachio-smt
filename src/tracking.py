5fbc992467565338d464bea953f5d0912c6825d4
Aaron Hopkinson <a.hopkinson@hey.com>
2021-02-12 20:59:54 +0000

    Prototype showing use of Ray for parallelization

--- src/tracking.py
+++ src/tracking.py
@@ -23,46 +23,49 @@ Version: 0.2.0
 
 import sys
 
+import ray
 import numpy as np
 
 import spots
 import trajectories
 
 
+@ray.remote
+def _single_frame_track(image_data, params, frame):
+    # Isolate this frame's data
+    frame_data = image_data[frame]
+    # frame_data.show()
+
+    # Find the spots in this frame
+    frame_spots = spots.Spots(frame=frame)
+    frame_spots.find_in_frame(frame_data.as_image()[:, :], params)
+    found_spots = frame_spots.num_spots
+    frame_spots.merge_coincident_candidates()
+
+    merged_spots = frame_spots.num_spots
+    # Iteratively refine the spot centres
+    frame_spots.refine_centres(frame_data, params)
+    print(
+        f"Frame {frame:4d}: found {frame_spots.num_spots:3d} spots "
+        f"({found_spots:3d} identified, "
+        f"{found_spots-merged_spots:3d} merged, "
+        f"{merged_spots-frame_spots.num_spots:3d} filtered)"
+    )
+    if params.render_image:
+        frame_data.render(params, spot_positions=frame_spots.positions)
+    frame_spots.get_spot_intensities(frame_data.as_image()[:, :])
+    return frame_spots
+
+
 def track(image_data, params):
+    ray.init()
     # For each frame, detect spots
-    all_spots = []
-    for frame in range(image_data.num_frames):
-
-        # Isolate this frame's data
-        frame_data = image_data[frame]
-        # frame_data.show()
-
-        # Find the spots in this frame
-        frame_spots = spots.Spots(frame=frame)
-        frame_spots.find_in_frame(frame_data.as_image()[:, :], params)
-        found_spots = frame_spots.num_spots
-        frame_spots.merge_coincident_candidates()
-
-        merged_spots = frame_spots.num_spots
-        # Iteratively refine the spot centres
-        frame_spots.refine_centres(frame_data, params)
-
-        frame_spots.filter_candidates(params)
-
-        print(
-            f"Frame {frame:4d}: found {frame_spots.num_spots:3d} spots "
-            f"({found_spots:3d} identified, "
-            f"{found_spots-merged_spots:3d} merged, "
-            f"{merged_spots-frame_spots.num_spots:3d} filtered)"
-        )
-
-        if params.render_image:
-            frame_data.render(params, spot_positions=frame_spots.positions)
-
-        all_spots.append(frame_spots)
-        frame_spots.get_spot_intensities(frame_data.as_image()[:, :])
-
+    all_spots = [
+        _single_frame_track.remote(image_data, params, frame)
+        for frame in range(image_data.num_frames)
+    ]
+    all_spots = ray.get(all_spots)
+    ray.shutdown()
     # Link the spot trajectories across the frames
     trajs = trajectories.build_trajectories(all_spots, params)
     trajectories.write_trajectories(trajs, params)
